91766eddbd486b6157975cf9795f3dde7a98b246
abumarasak <abumarask@gmail.com>
2021-11-16 17:38:59 +0100

    finsh front-end without react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Topbar from "./components/topbar/topbar";
 import Home from "./pages/home/home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+
 function App() {
   return (
     <>
       <Topbar />
-      <Settings />
+      <Home />
     </>
   );
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,31 @@
+import "./register.css";
+
+export default function Register() {
+  return (
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>Username</label>
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter Your Username..."
+        />
+        <label>Email</label>
+        <input
+          className="registerInput"
+          type="email"
+          placeholder="Enter Your Email..."
+        />
+        <label>Password</label>
+        <input
+          className="registerInput"
+          type="password"
+          placeholder="Enter Password Email..."
+        />
+        <button className="registerButton">Register</button>
+      </form>
+      <button className="registerLoginButton">Login</button>
+    </div>
+  );
+}
